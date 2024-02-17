43f887be35b5711159ad7ae314599ae18d6d4aa9
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-17 22:47:40 +0330

    menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -32,7 +32,7 @@ const menu = [
 const Menu = () => {
   return (
     <section className="relative py-12 xl:py-24 lg:bg-menu" id="menu">
-      <div className="container mx-auto">
+      <div className="container max-w-[1280px] mx-auto">
         <div>
           <h2>آیتم های محبوب</h2>
           <Link href="/" className="text-green">
@@ -43,9 +43,26 @@ const Menu = () => {
         <div className="grid grid-cols-1 gap-[30px] md:grid-cols-3 md:gap-[15px] xl:grid-cols-4">
           {menu.map((item, index) => {
             return (
-              <div key={index} className="max-w-[270px] bg-white shadow-primary mx-auto lg:mx-0 group">
-                <div>
-                  <img src={item.img} width={270} height={270} alt="." />
+              <div
+                key={index}
+                className="max-w-[270px] bg-white shadow-primary mx-auto lg:mx-0 group"
+              >
+                <div className="overflow-hidden">
+                  <Image
+                    src={item.img}
+                    width={270}
+                    height={270}
+                    alt="."
+                    className="group-hover:scale-110 duration-200"
+                  />
+                </div>
+                <div className="pt-[20px] pb-[28px] px-[30px]">
+                  <Link href="/">
+                    <h3 className="text-black mb-[14px]">{item.title}</h3>
+                  </Link>
+                  <div className="number text-xl font-semibold text-orange">
+                    {item.price}
+                  </div>
                 </div>
               </div>
             );
