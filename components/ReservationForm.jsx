736ca2842fcb4bb33036f910e2ad4b2356e46eff
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-18 23:22:55 +0330

    form resevation

--- components/ReservationForm.jsx
+++ components/ReservationForm.jsx
@@ -14,7 +14,7 @@ import { Label } from "./ui/label"
 import { Input } from "./ui/input"
 import {
     Select,
-    selectContent,
+    SelectContent,
     SelectGroup,
     SelectItem,
     SelectLabel,
@@ -26,8 +26,65 @@ import {
 
 
 const ReservationForm = () => {
+    const [date,setDate]= useState()
   return (
-    <div>ReservationForm</div>
+    <form className="flex flex-col gap-y-10">
+        <div className="grid gap-[30px]">
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-[30px]">
+                <div>
+                    <Label html-for="firstname">نام</Label>
+                    <Input type="text" id="firstname"/>
+                </div>
+                <div>
+                    <Label html-for="lastname">نام خانوادگی</Label>
+                    <Input type="text" id="lastname"/>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-[30px]">
+                <div>
+                    <Label>تاریخ</Label>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button 
+                            variant={"input"}
+                            className={cn("w-full justify-end text-left font-normal")}
+                            >
+                                <CalendarIcon className="ml-2 h-4 w-4"/>
+                                {date ? format(date,"PPP") : <span>تایین تاریخ</span>}
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0">
+                            <Calendar
+                            mode="single"
+                            selected={date}
+                            onSelect={setDate}
+                            initialFocuse
+                            />
+                        </PopoverContent>
+                    </Popover>
+                </div>
+                <div>
+                    <Label>نفرات</Label>
+                    <Select>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="تعداد نفرات"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>شخص</SelectLabel>
+                                <SelectItem value="1">1</SelectItem>
+                                <SelectItem value="2">2</SelectItem>
+                                <SelectItem value="3">3</SelectItem>
+                                <SelectItem value="4">4</SelectItem>
+                                <SelectItem value="5">5</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div>
+            </div>
+        </div>
+        <Button className="w-full ">رزرو</Button>
+    </form>
   )
 }
 
