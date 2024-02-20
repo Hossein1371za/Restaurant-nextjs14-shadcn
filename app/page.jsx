7ae611fee74789d41a755929a7211d1af98b8bd0
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-20 21:47:48 +0330

    start footer

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import Reservation from "@/components/Reservation";
 import About from "@/components/About";
 import Map from "@/components/Map";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
@@ -13,7 +14,7 @@ export default function Home() {
       <Reservation />
       <About />
       <Map />
-      <div className="h-[4000px]"></div>
+      <Footer />
     </main>
   );
 }
